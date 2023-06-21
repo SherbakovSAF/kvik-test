a441061d7e808a20a99d3e38757074f555bc8a50
SherbakovSAF <sherbakovsaf@gmail.com>
2023-06-21 16:37:27 +0300

    Реализация добавления таски через VueX

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,43 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    tasks: [
+      {
+        id: 0,
+        taskValue: "",
+      },
+      {
+        id: 1,
+        taskValue: "",
+      },
+      {
+        id: 2,
+        taskValue: "",
+      },
+    ],
   },
   getters: {
+    validID(state){
+      let localValue = 0
+      for (let i = 0; i < state.length; i++) {
+           if(state[i].id === i){
+                localValue++
+           } else {
+                return localValue
+           }       
+      }
+    }
   },
   mutations: {
+    addTask (state, task){
+      let localTask = {
+        id: getters.validID,
+        taskValue: task
+      }
+
+      state.unshift(localTask)
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
