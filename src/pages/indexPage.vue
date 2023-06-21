109756c5c4f3bd4538f9df034f1f5f02e6d4fc28
SherbakovSAF <sherbakovsaf@gmail.com>
2023-06-21 13:36:56 +0300

    Создание структуры проекта

---
+++ src/pages/indexPage.vue
@@ -0,0 +1,16 @@
+<template>
+     <div>
+          <h1>IndexPage</h1>
+     </div>
+</template>
+
+<script>
+export default {
+     name: "IndexPage",
+     data(){
+          return{
+
+          }
+     }
+}
+</script>
