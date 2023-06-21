4fab4e5b1e2f6ad7d3cfbc4e4ac0e830fe16be4e
SherbakovSAF <sherbakovsaf@gmail.com>
2023-06-21 13:58:59 +0300

    Отображение всех тасок на странице

--- src/components/tasksList.vue
+++ src/components/tasksList.vue
@@ -1,8 +1,8 @@
 <template>
      <div> <!-- Врапер для всех таксок -->
-          <div> <!-- name="Врапер для всей таски" v-for тут -->
+          <div v-for="tasks in localTasksList" :key="tasks.id"> <!-- name="Врапер для всей таски" v-for тут -->
                <div> <!-- name="Врапер для текста таски" -->
-                    <div>Задача</div>
+                    <div>{{tasks.taskValue}}</div>
                     <input type="text"> <!-- name="При редактировании будет инпут" -->
                </div>
                <div>  <!-- name="Врапер кнопок. Декоративный характер" -->
@@ -18,8 +18,20 @@ export default {
      name: "TasksList",
      data(){
           return{
-
+               localTasksList: [
+                    {
+                         id: 1,
+                         taskValue: 'Купить шторы'
+                    },
+                    {
+                         id: 2,
+                         taskValue: 'Не шторы'
+                    }
+               ]
           }
+     },
+     computed(){
+          //Получение таксок из Store... !!!Возможно лучше это делать в page и передавать пропсами. Смотри как будет обновляться всё
      }
 }
 </script>
